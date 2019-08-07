c99dd61178254f5b99af0a222a91f0d00d031b6e
XUWEI LIANG <email.xuwei@gmail.com>
2019-08-07 22:36:58 +1000

    initial commit

---
+++ CheqDemo/CheqDemo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CheqDemo
+//
+//  Created by XUWEI LIANG on 7/8/19.
+//  Copyright © 2019 WiseTree Solutions Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CheqDemo/CheqDemo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CheqDemo
+//
+//  Created by XUWEI LIANG on 7/8/19.
+//  Copyright © 2019 WiseTree Solutions Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
