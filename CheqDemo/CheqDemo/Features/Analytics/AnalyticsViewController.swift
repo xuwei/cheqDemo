a682a8ad0a7dff992a0f5b751019e2d566372b94
Xuwei <email.xuwei@gmail.com>
2019-08-11 23:21:41 +1000

    Develop (#4)
    * added comm bank theme
    
    * other viewcontrollers to update bg when theme switch

--- CheqDemo/CheqDemo/Features/Analytics/AnalyticsViewController.swift
+++ CheqDemo/CheqDemo/Features/Analytics/AnalyticsViewController.swift
@@ -12,19 +12,11 @@ class AnalyticsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         self.view.backgroundColor = sharedAppConfig.activeTheme.allBgColors().randomElement()
-        // Do any additional setup after loading the view.
+
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
